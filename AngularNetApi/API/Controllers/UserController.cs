275c54c0c78294fa00dfca091ce7ea74bd9ad54e
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-08-19 22:00:32 +0200

    a

--- AngularNetApi/API/Controllers/UserController.cs
+++ AngularNetApi/API/Controllers/UserController.cs
@@ -1,5 +1,4 @@
 ﻿using AngularNetApi.Application.MediatR.ProfileManagement.User.CreateUser;
-using AngularNetApi.Core.Exceptions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,31 +23,8 @@ namespace AngularNetApi.API.Controllers
                 ModelState.AddModelError(string.Empty, "Invalid registration attempt.");
                 return BadRequest(ModelState);
             }
-            try
-            {
-                return Ok(await _mediator.Send(request));
-            }
-            catch (BadRequestException ex)
-            {
-                return BadRequest(
-                    new { message = ex.Message, details = ex.InnerException?.Message }
-                );
-            }
-            catch (ServerErrorException ex)
-            {
-                // Gestione delle eccezioni generiche del server
-                return StatusCode(
-                    500,
-                    new { message = ex.Message, details = ex.InnerException?.Message }
-                );
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(
-                    500,
-                    new { message = ex.Message, details = ex.InnerException?.Message }
-                );
-            }
+
+            return Ok(await _mediator.Send(request));
         }
     }
 }
